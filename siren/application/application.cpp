c10b393f262e8ebca4d4896c2dbab1bf45595611
Arvid Wahlgren <salliban03@gmail.com>
2025-12-16 23:48:07 +0100

    Test(Minaudio & WavDecoder): Created testing scernario for playing a WAV file using Miniaudio

--- siren/application/application.cpp
+++ siren/application/application.cpp
@@ -3,17 +3,54 @@
 
 #include <iostream>
 #include <vector>
+#include <span>
+#include <thread>
+
+// Minaudio
+#define MINIAUDIO_IMPLEMENTATION
+#include "../external/miniaudio/miniaudio.h"
 
 // Quick Testing
 #include "siren/io/FileDataSource.h"
 #include "siren/io/MemoryDataSource.h"
+#include "siren/codecs/WavDecoder.h"
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+	// Get decoder stored in pUserData
+	auto* decoder = static_cast<siren::WavDecoder*>(pDevice->pUserData);
+	if (!decoder) {
+		return;
+	}
+
+	// Prep destination buffer
+	float* outputBuffer = static_cast<float*>(pOutput);
+
+	size_t channelCount = decoder->getChannelCount();
+	size_t totalSamplesNeeded = frameCount * channelCount;
+
+	// Create buffer for decoder to fill
+	std::span<float> buffer(outputBuffer, totalSamplesNeeded);
+
+	size_t framesDecoded = decoder->decode(buffer);
+
+	// Handle EOF (Audio clip ended)
+	if (framesDecoded < frameCount) {
+		size_t samplesDecoded = framesDecoded * channelCount;
+		size_t samplesRemaining = totalSamplesNeeded - samplesDecoded;
+
+		// Set remaining samples to 0.0f (silenece)
+		std::fill_n(outputBuffer + samplesDecoded, samplesRemaining, 0.0f);
+	}
+}
+
+using namespace siren;
 
 int main() {
 	siren::HelloWorld();
 	std::cout << std::endl;
 
-	{
-		// FileDataSource
+	// FileDataSource
+	if (false) {
 		std::cout << "-- TESTING [FileDataSource] --" << std::endl;
 
 		siren::FileDataSource fileTest("assets/test.txt");
@@ -32,8 +69,8 @@ int main() {
 		std::cout << std::endl << std::endl;
 	}
 
-	{
-		// MemoryDataSource
+	// MemoryDataSource
+	if (false) {
 		std::cout << "-- TESTING [MemoryDataSource] --" << std::endl;
 
 		std::string text = "Testing";
@@ -51,6 +88,52 @@ int main() {
 		std::cout << "Buffer: ";
 		std::cout.write(reinterpret_cast<const char*>(memoryBuffer.data()), memoryBuffer.size());
 		std::cout << std::endl << std::endl;
+	}
+
+	// Audio playtest
+	if (true) {
+		// Engine setup
+		const char* filename = "assets/white_ferrari.wav";
+		auto source = std::make_unique<FileDataSource>(filename);
+		auto decoder = std::make_unique<WavDecoder>();
+
+		std::cout << "Priming decoder for: " << filename << std::endl;
+		if (decoder->prime(std::move(source)) != ResultCode::Success) {
+			std::cout << "Decoder priming failed" << std::endl;
+			return -1;
+		}
+		
+		std::cout << "Decoder priming finished" << std::endl;
+		std::cout << "Channel count: " << decoder->getChannelCount() << " Hz" << std::endl;
+		std::cout << "Sample rate: " << decoder->getSampleRate() << std::endl;
+
+		// Miniaudio setup
+		ma_device_config config = ma_device_config_init(ma_device_type_playback);
+
+		// Matching config to WAV format
+		config.playback.format = ma_format_f32;
+		config.playback.channels = decoder->getChannelCount();
+		config.sampleRate = decoder->getSampleRate();
+
+		config.dataCallback = data_callback;
+		config.pUserData = decoder.get();
+
+		ma_device device;
+		if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) {
+			std::cout << "Failed to init audio device" << std::endl;
+			return -2;
+		}
+
+		if (ma_device_start(&device) != MA_SUCCESS) {
+			std::cout << "Failed to start playback" << std::endl;
+			ma_device_uninit(&device);
+			return -3;
+		}
+
+		std::cout << "Playing... [Enter] to exit" << std::endl;
+		getchar();
+
+		ma_device_uninit(&device);
 	}
 
 	return 0;
