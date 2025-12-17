4a076f7d3267e3a85d1fa6fe08e9dc371485f943
Arvid Wahlgren <salliban03@gmail.com>
2025-12-17 22:59:54 +0100

    Feat(Voice): Decoder attaching and primitive process functionality

---
+++ siren/siren/src/Voice.cpp
@@ -0,0 +1,77 @@
+#include "siren/Voice.h"
+
+namespace siren {
+
+	void Voice::attachDecoder(std::unique_ptr<Decoder> decoder) {
+		m_decoder = std::move(decoder);
+	}
+
+	void Voice::process(std::span<float> dst) {
+		// Has to be somewhat thread-safe
+
+		// TODO: Implement support for Mono. For mono to stereo up-mixing,
+		// we will need an intermediate buffer to hold the raw mono before expanding it to stereo
+
+		// Getting all variables at once (for thread safety)
+		float volume = m_volume;
+		float pan = m_pan;
+		bool isLooping = m_isLooping;
+		size_t channelCount = m_decoder->getChannelCount();
+
+		if (m_state != VoiceState::Playing || !m_decoder) {
+			std::fill_n(dst, dst.size(), 0.0f);
+			return;
+		}
+
+		size_t framesRequested = dst.size() / channelCount;
+		size_t framesRead = 0;
+
+		while (framesRead < framesRequested) {
+
+			size_t framesRemaining = framesRequested - framesRead;
+			size_t samplesRead = framesRead * channelCount;
+			size_t samplesRemaining = framesRemaining * channelCount;
+
+			std::span<float> buffer = dst.subspan(samplesRead, samplesRemaining);
+			framesRead += m_decoder->decode(buffer);
+
+			if (framesRead < framesRequested) {
+				// Hit EOF
+				if (m_isLooping) {
+					// Continue filling the buffer from the start
+					m_decoder->seek(0);
+					continue;
+				}
+				else {
+					// Fill rest of the buffer with silence
+					framesRemaining = framesRequested - framesRead;
+					samplesRead = framesRead * channelCount;
+					samplesRemaining = framesRemaining * channelCount;
+					std::fill_n(dst.data() + (samplesRead), samplesRemaining, 0.0f);
+					m_state = VoiceState::Inactive;
+					break;
+				}
+			}
+		}
+
+		for (size_t i = 0; i < dst.size(); i++) {
+			// Apply volume
+			dst[i] *= volume;
+
+			if (channelCount == 2) {
+				// Apply pan
+				if (i % 2 == 0) { // Left
+					dst[i] *= (1.0 - pan);
+				}
+				else { // Right
+					dst[i] *= (1.0 + pan);
+				}
+			}
+		}
+	}
+
+
+
+}
+
+
