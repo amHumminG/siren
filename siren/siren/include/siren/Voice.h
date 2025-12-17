89a279f3024e45db74a1525fccfcc4dc3fb9d4a2
Arvid Wahlgren <salliban03@gmail.com>
2025-12-17 17:03:11 +0100

    Feat(Voice): Started work on Voice class

---
+++ siren/siren/include/siren/Voice.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "siren/DecoderFactory.h"
+
+enum VoiceState {
+	Stopped,
+	Playing,
+	Paused
+};
+
+namespace siren {
+
+	class Voice {
+	private:
+		std::unique_ptr<Decoder> decoder;
+
+		VoiceState m_state = VoiceState::Stopped;
+		
+		float m_volume = 1.0f;
+		float m_pan = 0.0f;
+		bool m_isLooping = false;
+
+	public:
+		Voice() = default;
+		~Voice() = default;
+
+		[[nodiscard]] ResultCode attachDecoder(std::unique_ptr<Decoder> decoder);
+
+
+		[[nodiscard]] size_t process(std::span<std::byte> dst);
+
+		void play();
+		void pause();
+		void stop(); // Should this have a return value?
+
+		void setVolume(const float& value);
+		void setPan(const float& value);
+		void setLooping(const bool& value);
+
+		[[nodiscard]] float getVolume();
+		[[nodiscard]] float getPan();
+		[[nodiscard]] bool isLooping();
+
+		[[nodiscard]] bool isPlaying();
+	};
+}
