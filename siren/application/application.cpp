2ca469dce7768a3b500e2437550a21206dad9274
Arvid Wahlgren <salliban03@gmail.com>
2025-12-19 21:29:08 +0100

    Test(Sytstem): Multiple sounds and seeking

--- siren/application/application.cpp
+++ siren/application/application.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <span>
 #include <thread>
+#include <chrono>
+#include <random>
 
 // Testing
 #include "siren/AudioContext.h"
@@ -15,29 +17,59 @@ using namespace siren;
 int main() {
 	std::cout << "SIREN SANDBOX" << std::endl << std::endl;
 
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+
 	AudioContext context;
 	context.init();
-	Sound sound = Sound::Stream("assets/white_ferrari.wav");
-	if (!sound.isValid()) {
-		std::cerr << "Invalid sound" << std::endl;
-		return -1;
-	}
-	std::shared_ptr<Voice> voice = context.play(sound);
+	Sound whiteFerrari = Sound::Stream("assets/white_ferrari.wav", "White Ferrari - ANDREWBATES");
+	Sound metalPipe = Sound::Internal("assets/metal_pipe.wav", "Metal Pipe Falling");
+
+	std::shared_ptr<Voice> voice1 = context.play(whiteFerrari);
 	//std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	//std::shared_ptr<Voice> voice2 = context.play(sound);
 
 	// Test panning
 	float increment = 0.002;
-	while (voice->isPlaying()) {
-		float currentPan = voice->getPan();
-		if (currentPan < -0.9 || currentPan > 0.9) {
-			increment = -increment;
-		}
-		voice->setPan(currentPan + increment);
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-	}
 
-	std::cin.get();
+	using Clock = std::chrono::steady_clock;
+	auto lastTime = Clock::now();
+	float pipeTimer = 0.0f;
+	float seekTimer = 0.0f;
+	
+	std::chrono::milliseconds deltaTime(0);
+	while (true) {
+		// deltaTime
+		auto currentTime = Clock::now();
+		std::chrono::duration<float> fs = currentTime - lastTime;
+		float deltaTime = fs.count();
+		lastTime = currentTime;
+
+		pipeTimer += deltaTime;
+		if (pipeTimer >= 5.0f) {
+			std::shared_ptr<Voice> voice = context.play(metalPipe);
+			voice->setPan(dist(gen));
+
+			pipeTimer -= 5.0f;
+		}
+
+		seekTimer += deltaTime;
+		if (seekTimer >= 3.0f) {
+			voice1->seek(44400.0f);
+
+			seekTimer -= 3.0f;
+		}
+
+		if (voice1->isPlaying()) {
+			float currentPan = voice1->getPan();
+			if (currentPan < -0.9 || currentPan > 0.9) {
+				increment = -increment;
+			}
+			voice1->setPan(currentPan + increment);
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
+	}
 
 	context.deinit();
 
