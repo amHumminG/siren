fc96c4bcd0e7a8f663f0394a5b16be61deef1700
Arvid Wahlgren <salliban03@gmail.com>
2025-12-19 12:41:58 +0100

    Docs(AudioContext): Doxygen for member functions

--- siren/siren/include/siren/AudioContext.h
+++ siren/siren/include/siren/AudioContext.h
@@ -22,15 +22,28 @@ namespace siren {
 
 		std::vector<std::shared_ptr<Voice>> m_voiceRegistry; // Only accessed by data_callback
 
+		/// @brief Writes audio data to the device
+		/// @param pDevice The device
+		/// @param pOutput Buffer for the audio data to be written to
+		/// @param pInput Unused (used for audio recording)
+		/// @param frameCount The number of frames requested by the audio device
 		static void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, uint32_t frameCount);
 
 	public:
 		AudioContext();
 		~AudioContext();
 
+		/// @brief Initializes the AudioContext
+		/// @return True if intialization was successful, otherwise false
 		bool init();
+
+		/// @brief Deinitializes the AudioContext
+		/// @return True if deinitialization was successful, otherwise false
 		bool deinit();
 
+		/// @brief Plays a sound
+		/// @param sound The sound to be played
+		/// @return A shared pointer to the voice that has been created to play the sound
 		std::shared_ptr<Voice> play(const Sound& sound);
 	};
 }
