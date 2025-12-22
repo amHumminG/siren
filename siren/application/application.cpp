77e2f78880d15584a7cfb15d4f34b05a1b5329ec
Arvid Wahlgren <salliban03@gmail.com>
2025-12-22 22:37:08 +0100

    Test(System): Better testing scenario

--- siren/application/application.cpp
+++ siren/application/application.cpp
@@ -21,27 +21,39 @@ int main() {
 	std::mt19937 gen(rd());
 	std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
 
+	// Context creation
 	AudioContext context;
 	context.init();
+
+	// Bus creation
+	context.createBus("Music");
+	context.createBus("SFX");
+	context.setBusVolume("SFX", 0.3f);
+	context.setBusVolume("Master", 0.7f);
+
+	// Sound creation
 	Sound whiteFerrari = Sound::Stream("assets/white_ferrari.wav", "White Ferrari - ANDREWBATES");
+	Sound bloodRunWarm = Sound::Internal("assets/blood_run_warm.wav", "Blood Run Warm - South Arcade");
 	Sound metalPipe = Sound::Internal("assets/metal_pipe.wav", "Metal Pipe Falling");
 
-	std::shared_ptr<Voice> voice1 = context.play(whiteFerrari);
+	// Sound playback
+	std::shared_ptr<Voice> voice1 = context.play(whiteFerrari, "Music");
 	//std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	//std::shared_ptr<Voice> voice2 = context.play(whiteFerrari);
+	std::shared_ptr<Voice> voice2 = context.play(bloodRunWarm, "Master");
+	voice2->setLooping(true);
 
-	// Test panning
-	float increment1 = 0.002;
-	float increment2 = -increment1 * 2;
+	// Ping pong testing
+	float angle = 0.0f;
+	float frequency = 0.1f;
+
+	// Timers
+	float pipeTimer = 0.0f;
 
 	using Clock = std::chrono::steady_clock;
 	auto lastTime = Clock::now();
-	float pipeTimer = 0.0f;
-	float seekTimer = 0.0f;
-	
 	std::chrono::milliseconds deltaTime(0);
 	while (true) {
-		// deltaTime
+		// Delta Time
 		auto currentTime = Clock::now();
 		std::chrono::duration<float> fs = currentTime - lastTime;
 		float deltaTime = fs.count();
@@ -49,29 +61,33 @@ int main() {
 
 		pipeTimer += deltaTime;
 		if (pipeTimer >= 5.0f) {
-			std::shared_ptr<Voice> voice = context.play(metalPipe);
+			std::shared_ptr<Voice> voice = context.play(metalPipe, "SFX");
 			voice->setPan(dist(gen));
+
+			if (voice1) {
+				if (voice1->isPlaying()) {
+					voice1->pause();
+				}
+				else {
+					voice1->play();
+				}
+			}
 
 			pipeTimer -= 5.0f;
 		}
 
-		//seekTimer += deltaTime;
-		//if (seekTimer >= 3.0f) {
-		//	voice1->seek(44400.0f);
-
-		//	seekTimer -= 3.0f;
-		//}
+		// Ping Pong logic
+		angle += frequency * 6.28318f * deltaTime;
+		float volSine = std::sin(angle);
+		
+		// Volume Ping Pong
+		float volume = (volSine + 1.0f) * 0.5f;
+		context.setBusVolume("Music", volume);
 
 		if (voice1->isPlaying()) {
-			float currentPan1 = voice1->getPan();
-			//float currentPan2 = voice2->getPan();
-			if (currentPan1 < -0.9 || currentPan1 > 0.9) {
-				increment1 = -increment1;
-				increment2 = -increment2;
-			}
-			voice1->setPan(currentPan1 + increment1);
-			//voice2->setPan(currentPan2 + increment2);
-			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			// Pan Ping Pong
+			float pan = (volSine + 1.0f) - 1.0f;
+			voice1->setPan(pan);
 		}
 	}
 
