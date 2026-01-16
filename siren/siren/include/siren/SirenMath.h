bc10d5b490915cb36e6788f146994b89916a9340
Arvid Wahlgren <salliban03@gmail.com>
2026-01-16 19:39:55 +0100

    Feat(Vector3): Addition operator

--- siren/siren/include/siren/SirenMath.h
+++ siren/siren/include/siren/SirenMath.h
@@ -16,6 +16,10 @@ namespace siren {
 			: x(x), y(y), z(z) {
 		}
 
+		Vector3 operator+(const Vector3& other) const {
+			return Vector3(x + other.x, y + other.y, z + other.z);
+		}
+
 		Vector3 operator-(const Vector3& other) const {
 			return Vector3(x - other.x, y - other.y, z - other.z);
 		}
